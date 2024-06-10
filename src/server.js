87062b77f2ac2506f25d467b5b7bdc040f9751ed
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-06-10 05:39:20 -0300

    node server raw to add users into db json

---
+++ src/server.js
@@ -0,0 +1,50 @@
+//const http = require('http')
+
+// CommonJS => require
+// ESModule => import/export
+// By default node did not support ESModule
+/**
+ * Add "type": "module", to the package json
+ */
+
+
+// Stateful - Stateless
+
+// Headers => Metadata 
+
+import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+// Query parameters
+// Route parameters
+// Request body
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const routeParams = req.url.match(route.path)
+        
+        console.log(extractQueryParams(routeParams.groups.query))
+        console.log(routeParams.groups)
+
+        const { query, ...params } = routeParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
+
+        return route.handler(req, res)
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333)
